c9ab9c4ffbf932eeaef58ae68af18475cad53cc4
supet1337 <petr.vorontsov2003@gmail.com>
2023-04-24 17:37:51 +0300

    CreateSnippet allow only POST
    showSnippet returns smippets id

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Создается функция-обработчик "home", которая записывает байтовый слайс, содержащий
@@ -22,14 +24,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("Отображение заметки"))
-	if err != nil {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 0 {
+		http.NotFound(w, r)
 		log.Fatal(err)
 		return
 	}
+
+	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Метод запрещен!", 405)
+		return
+	}
 	_, err := w.Write([]byte("Создание заметки"))
 	if err != nil {
 		log.Fatal(err)
@@ -41,7 +52,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet", showSnippet) //localhost/snippet?id=123.
 	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")

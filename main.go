606c8c36370891189599a5f24896081921ffee10
supet1337 <petr.vorontsov2003@gmail.com>
2023-04-24 16:52:28 +0300

    Добавление новых хендлеров

--- main.go
+++ main.go
@@ -8,8 +8,29 @@ import (
 // Создается функция-обработчик "home", которая записывает байтовый слайс, содержащий
 // текст "Привет из Snippetbox" как тело ответа.
 func home(w http.ResponseWriter, r *http.Request) {
-	r.
-		_, err := w.Write([]byte("Привет из Snippetbox"))
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	_, err := w.Write([]byte("Привет из Snippetbox"))
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Отображение заметки"))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Создание заметки"))
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -20,6 +41,8 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)

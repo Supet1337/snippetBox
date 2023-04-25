cbac8a7264a853b67b01a42c97e9ef000d31a0a2
supet1337 <petr.vorontsov2003@gmail.com>
2023-04-25 15:58:26 +0300

    add custom errors

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -22,39 +21,39 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	templ, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.errorLog.Println(err.Error())
+		app.serverError(w, err)
 		return
 	}
 
 	err = templ.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.errorLog.Println(err.Error())
+		app.serverError(w, err)
 	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 0 {
-		http.NotFound(w, r)
-		log.Fatal(err)
+		app.notFound(w)
+		app.errorLog.Println(err.Error())
 		return
 	}
 
 	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Метод запрещен!", 405)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	_, err := w.Write([]byte("Создание заметки"))
 	if err != nil {
-		log.Fatal(err)
+		app.errorLog.Println(err.Error())
 		return
 	}
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	//Запись логов в файл
 	//logFile, err := os.OpenFile("info.log", os.O_RDWR|os.O_CREATE, 0666)
@@ -20,10 +25,16 @@ func main() {
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
 
 	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
 	flag.Parse()

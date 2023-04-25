464512e2e7fd3391e55ab3f49bdd75e49d811854
supet1337 <petr.vorontsov2003@gmail.com>
2023-04-25 14:33:14 +0300

    add css/js and logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	//Запись логов в файл
+	//logFile, err := os.OpenFile("info.log", os.O_RDWR|os.O_CREATE, 0666)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer logFile.Close()
+	//
+	//infoLog := log.New(logFile, "INFO\t", log.Ldate|log.Ltime)
+	//errorLog := log.New(logFile, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	log.Println("Запуск сервера на http://127.0.0.1:4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
+	flag.Parse()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Запуск сервера на %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }

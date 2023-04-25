063bb7beb32ef2ec2402bddd22e224403828aafe
supet1337 <petr.vorontsov2003@gmail.com>
2023-04-25 16:07:45 +0300

    created file routes go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -31,24 +31,16 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
 	addr := flag.String("addr", ":4000", "Сетевой адрес HTTP")
 	flag.Parse()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
-	infoLog.Printf("Запуск сервера на %s", *addr)
+	app.infoLog.Printf("Запуск сервера на %s", *addr)
 	err := srv.ListenAndServe()
-	errorLog.Fatal(err)
+	app.errorLog.Fatal(err)
 }

7ce0289c3d0ee6a7f72d60c863eab9eed7d023b1
supet1337 <petr.vorontsov2003@gmail.com>
2023-04-28 19:41:58 +0300

    TODO

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,9 @@
 package main
 
+//TODO Create snippet brom https body (unmarshall/marshal)
+// Edit date in snipet showing (......UTC 0000 ->  2023/04/28 19:37:35)
+// Add edit, delete handler
+// search
 import (
 	"database/sql"
 	"flag"
